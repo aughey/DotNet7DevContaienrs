b550339c5badefc12846c85fa3b6c0ea0931426e
John Aughey <jha@aughey.com>
2022-10-31 02:30:03 +0000

    foo

---
+++ App/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+
+Console.WriteLine("Hello, World!");
+
+System.Console.WriteLine("blech");
+
+// connect to localhost:12345
+var client = new TcpClient("localhost", 12345);
